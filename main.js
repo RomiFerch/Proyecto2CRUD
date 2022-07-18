236c9561f0b5a1710fc8b8e1258e4ce5210d128e
RomiFerCh <romi.fer@icloud.com>
2022-07-18 17:40:07 -0400

    faltan botones

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 //Declaración Variables
-let formulario1 = document.getElementById('formulario')
+let formulario1 = document.querySelector('formulario')
 let nombre = document.getElementById('descripcion')
 let price = document.getElementById('precio')
 let cantidad = document.getElementById('stock')
 let boton = document.getElementById('btnCrear')
-let arrayArticulos = []
 let tabla1 = document.getElementById('tabla1')
+
+let arrayArticulos = []
 
 //Funciones
 const agregarProducto = () =>{
@@ -16,34 +17,38 @@ const agregarProducto = () =>{
 
     }
     arrayArticulos.push(producto)
-    llenarTabla()
+ 
+    guardarLS()
+    
+
     
 }
 const guardarLS = () => {
     localStorage.setItem('products', JSON.stringify(arrayArticulos))
 
     llenarTabla()
+    
 }
 
 const llenarTabla = () =>{
     
     tabla1.innerHTML=''
 
-    //arrayArticulos = JSON.parse(localStorage.getItem('products'))
+    arrayArticulos = JSON.parse(localStorage.getItem('products'))
 
     if (arrayArticulos === null){
         arrayArticulos = []
     }else {
         arrayArticulos.forEach((producto, index) => {
             tabla1.innerHTML+= `
-            <tr>
+            <tr class="mt-4 ">
                 <td>${index +1}</td>
                 <td>${producto.descripcion}</td>
-                <td>${producto.precio}</td>
+                <td>$${producto.precio}</td>
                 <td>${producto.stock}</td>
                 <td>
-                    <span class="mx-3 float-end">
-                        <i class="fa-solid editar fa-pen-to-square mx-3"></i>
+                    <span class="float-auto">
+                        <i class="fa-solid edit fa-pen-to-square mx-3"></i>
                         <i class="fa-solid trash fa-trash-can mx-3"></i>
                     </span>
                 </td>
@@ -51,31 +56,40 @@ const llenarTabla = () =>{
             `
     
         })
+       
         
     }
-
-       
     }
     
    
-    accionBtn()
+   
 
 
 function accionBtn(){
-    let btnEditar = Array.from(document.getElementsByClassName('editar'))
-    let btnTrash = Array.from(document.getElementsByClassName('trash'))
+    let btnEditar = Array.from(document.getElementsByClassName('edit'))
+    let btnTrash = Array.from(document.getElementsByClassName('fa-solid trash fa-trash-can mx-3'))
     
-  
+    document.addEventListener('DOMContentLoaded', llenarTabla)
+
+const deleteBtn = (dBtn) =>{
+    btnTrash.addEventListener('click' (accionBtn))
+    accionBtn.prevenDefault()
+
+    console.log(accionBtn);
+}
+    accionBtn()
 }
 
 //console.log(btnEditar);
 //falta los agregar eventListener
 
-boton.addEventListener('click', agregarProducto)
 
-const verForm = () => {
+
+/*const verForm = () => {
     descripcion.innerHTML = '';
-}
+}*/
 
 
 llenarTabla()
+boton.addEventListener('click', agregarProducto)
+
