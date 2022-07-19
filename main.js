0db61c9c450ae7bf8598dc53a2d0bc3ded1d750f
RomiFerCh <romi.fer@icloud.com>
2022-07-19 17:58:15 -0400

    agregando eventos

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 //Declaración Variables
-let formulario1 = document.querySelector('formulario')
+let form = document.getElementById('formulario')
 let nombre = document.getElementById('descripcion')
 let price = document.getElementById('precio')
 let cantidad = document.getElementById('stock')
-let boton = document.getElementById('btnCrear')
+let btnAdd = document.getElementById('btnCrear')
 let tabla1 = document.getElementById('tabla1')
 
 let arrayArticulos = []
 
-//Funciones
+//Funcion para tomar los datos y agregarlos al array como objeto = producto
 const agregarProducto = () =>{
     let producto = {
         descripcion: nombre.value,
@@ -17,19 +17,20 @@ const agregarProducto = () =>{
 
     }
     arrayArticulos.push(producto)
- 
+    
     guardarLS()
-    
-
-    
+    //form.reset()
+//la funcioón para resetar el for,mulario no se por no me funciona :´(
 }
+//Funcion para levar los datos del Array al LocalStorage
 const guardarLS = () => {
     localStorage.setItem('products', JSON.stringify(arrayArticulos))
 
     llenarTabla()
     
 }
-
+//funcion para llenar tabla en el tbody del HTML
+//quisiera poner condicionales pero no se como :(
 const llenarTabla = () =>{
     
     tabla1.innerHTML=''
@@ -59,23 +60,34 @@ const llenarTabla = () =>{
        
         
     }
-    }
     
-   
-   
+    }
 
+document.addEventListener('DOMContentLoaded', llenarTabla)
+
+//Funcion para darle accion a los botones del formulario
+//Diosito iluminame
 
 function accionBtn(){
-    let btnEditar = Array.from(document.getElementsByClassName('edit'))
+    let btnEditar = Array.from(document.getElementsByClassName('fa-solid edit fa-pen-to-square mx-3'))
     let btnTrash = Array.from(document.getElementsByClassName('fa-solid trash fa-trash-can mx-3'))
-    
-    document.addEventListener('DOMContentLoaded', llenarTabla)
 
-const deleteBtn = (dBtn) =>{
+    btnTrash.forEach((deleteBtn)=>{
+        const newLocal = 'hola'
+        deleteBtn.addEventListener('click', console.log(newLocal))
+    })
+}
+
+    
+/*const deleteBtn = (dBtn) =>{
     btnTrash.addEventListener('click' (accionBtn))
     accionBtn.prevenDefault()
+    
+//     if(Element.arrayArticulos === dBtn){
+//         arrayArticulos.splice
+//     }
 
-    console.log(accionBtn);
+//     console.log(accionBtn);
 }
     accionBtn()
 }
@@ -91,5 +103,7 @@ const deleteBtn = (dBtn) =>{
 
 
 llenarTabla()
-boton.addEventListener('click', agregarProducto)
+btnAdd.addEventListener('click', agregarProducto)
+
+agregarProducto.prevenDefault
 
